f2ba2050403b2e5df4e8001eae03c866bd24c6db
Eric w <none>
2024-08-14 12:43:55 -0700

    adding start files

---
+++ src/test/java/com/adopt/adopt/AdoptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adopt.adopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
